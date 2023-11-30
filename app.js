f26d40b24d35a31f9e747bb994751a245c4ac143
Diego Lopes <diegolopes087@gmail.com>
2023-11-30 16:12:38 -0300

    feat: mostra lista repositórios do usuário

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 $(document).ready(function () {
-    $('#search').click(function () {
-        const username = $('#username').val()
-        if (username === '') return
-
-        getUserProfile(username)
-    })
-})
+  $("#search").click(function () {
+    var username = $("#username").val();
+    if (username === "") return;
+    username = "octocat";
+    getUserProfile(username);
+    getUserRepos(username);
+  });
+});
 
 async function getUserProfile(username) {
-    try {
-        const response = await axios(`https://api.github.com/users/${username}`)
-        console.log(response.data)
+  const apiUrl = `https://api.github.com/users/${username}`;
 
-        displayUserProfile(response.data)
-        console.log
-    } catch {
-        console.error('Erro na requisição')
-        displayError()
-    }
+  try {
+    const response = await axios(apiUrl);
+
+    displayUserProfile(response.data);
+  } catch {
+    console.error("Erro na requisição");
+    displayError();
+  }
 }
-
 function displayUserProfile(user) {
-    const { avatar_url, login, bio, html_url, email, followers, following } =
-        user
+  const { avatar_url, login, bio, html_url, email, followers, following } =
+    user;
 
-    const profileContainer = $('#profile-container')
-    profileContainer.empty()
+  console.log(user);
 
-    const profileCard = `
+  const profileContainer = $("#profile-container");
+  profileContainer.empty();
+
+  const profileCard = `
     <div class="card">
         <div class="row">
             <div class="col-md-6">
@@ -36,8 +38,8 @@ function displayUserProfile(user) {
             <div class="col-md-6">
                 <div class="card-body">
                     <h5 class="card-title">${login}</h5>
-                    <p class="card-text">${email || 'email indisponível'}</p>
-                    <p class="card-text">${bio || 'bio indisponível'}</p>
+                    <p class="card-text">${email || "email indisponível"}</p>
+                    <p class="card-text">${bio || "bio indisponível"}</p>
                     <p class="card-text">nº de seguidores: ${followers}</p>
                     <p class="card-text">Seguindo ${following} usuários</p>                    
                     <a href="${html_url}" class="btn btn-primary" target="_blank">Ver perfil</a>
@@ -45,16 +47,44 @@ function displayUserProfile(user) {
             </div>
         </div>
     </div>
-`
+`;
 
-    profileContainer.append(profileCard)
+  profileContainer.append(profileCard);
+}
+
+async function getUserRepos(username) {
+  const repoUrl = `https://api.github.com/users/${username}/repos?sort=stargazers_count&direction=desc`;
+
+  try {
+    const response = await axios(repoUrl);
+
+    displayUserRepos(response.data);
+  } catch {
+    console.error("Erro na requisição");
+    displayError();
+  }
+}
+
+function displayUserRepos(repos) {
+  const reposContainer = $("#repos-container");
+  reposContainer.empty();
+
+  $("#repos-list").empty();
+
+  $.each(repos, function (index, item) {
+    $("#repos-list").append(`
+    <li class='list-group-item d-flex'>
+        <div>
+            <span>${item.full_name}</span></br>
+            <span>Stars:${item.stargazers_count}</span>
+        </div>
+    </li>`);
+  });
 }
 
 function displayError() {
-    const profileContainer = $('#profileContainer')
-    profileContainer.empty()
+  const profileContainer = $("#profileContainer");
+  profileContainer.empty();
 
-    profileContainer.append(
-        '<p class="text-danger">Usuário não encontrado.</p>'
-    )
+  profileContainer.append('<p class="text-danger">Usuário não encontrado.</p>');
 }
